5e6cac353db38a020760aeb7534cdbcc268eb19c
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2023-05-05 20:18:52 +0200

    Update process_gtf.py

--- process_gtf.py
+++ process_gtf.py
@@ -9,14 +9,14 @@ db = sys.argv[2]
 for line in coordinates:
   try:
     chr = line.split("\t")[0]
-    start = min(line.split("\t")[1], line.split("\t")[2])
-    end = max(line.split("\t")[1],line.split("\t")[2])
-    key = line.split("\t")[3]
+    start = min(int(line.split("\t")[1]), int(line.split("\t")[2]))
+    end = max(int(line.split("\t")[1]),int(line.split("\t")[2]))
+    key = line.split("\t")[3].strip()
     
     header = "#{0},position={1}:{2}-{3}".format(key, chr, start, end)
     urls = "http://genome.ucsc.edu/cgi-bin/hgTracks?db={0}&position={1}:{2}-{3}".format(db, chr, start, end)
-    links.write(header + "\n" + urls + tracks + "\n")
-  except e:
+    links.write(header + "\n" + urls + "\n")
+  except:
     print("Unexpected file format. BED file supported.")
 
 links.close()
