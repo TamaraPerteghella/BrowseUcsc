bf791a2cfbb56c4643b3ea94aff9e1a38397deef
TamaraPerteghella <25196661+TamaraPerteghella@users.noreply.github.com>
2023-05-05 20:02:53 +0200

    Create process_gtf.py

---
+++ process_gtf.py
@@ -0,0 +1,22 @@
+import pickle
+import sys
+
+#BED file containing the coordinates to display
+coordinates = open(sys.argv[1], "r")
+links = open("display_ucsc.urls.txt", "w")
+db = sys.argv[2]
+
+for line in annotation_set:
+  try:
+    chr = line.split("\t")[0]
+    start = min(line.split("\t")[1], line.split("\t")[2])
+    end = max(line.split("\t")[1],line.split("\t")[2])
+    key = line.split("\t")[3]
+    
+    header = "#{0},position={1}:{2}-{3}".format(key, chr, start, end)
+    urls = "http://genome.ucsc.edu/cgi-bin/hgTracks?db={0}&position={1}:{2}-{3}".format(db, chr, start, end)
+    links.write(header + "\n" + urls + tracks + "\n")
+  except e:
+    print("Unexpected file format. BED file supported.")
+
+links.close()
